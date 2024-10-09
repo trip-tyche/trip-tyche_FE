9a74743e909172366f7a97fb2f4e351ba4ed9e72
Mark Kwon <redhero8830@gmail.com>
2024-10-10 02:59:01 +0900

    design: 온보딩 페이지 애니메이션 제거

--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 
-import { css, keyframes } from '@emotion/react';
+import { css } from '@emotion/react';
+// import { css, keyframes } from '@emotion/react';
 import { Navigate } from 'react-router-dom';
 
 import imageLeft from '@/assets/images/ogami_1.png';
@@ -27,14 +28,10 @@ const Onboarding = () => {
         <div css={containerStyle}>
             <div css={contentStyle}>
                 <div css={imageContainerStyle}>
-                    <div css={imageSlideStyle}>
-                        <img css={imageStyle} src={imageLeft} alt='image-left' />
-                        <img css={imageStyle} src={imageRight} alt='image-right' />
-                        <img css={imageStyle} src={imageLeft} alt='image-left' />
-                        <img css={imageStyle} src={imageRight} alt='image-right' />
-                        <img css={imageStyle} src={imageLeft} alt='image-left' />
-                        <img css={imageStyle} src={imageRight} alt='image-right' />
-                    </div>
+                    {/* <div css={imageSlideStyle}> */}
+                    <img css={imageStyle} src={imageLeft} alt='image-left' />
+                    <img css={imageStyle} src={imageRight} alt='image-right' />
+                    {/* </div> */}
                 </div>
                 <div>
                     <h1 css={titleStyle}>여행을 통해 추억을 남기다</h1>
@@ -45,14 +42,14 @@ const Onboarding = () => {
     );
 };
 
-const slideAnimation = keyframes`
-    0% {
-        transform: translateX(0);
-    }
-    100% {
-        transform: translateX(-50%);
-    }
-`;
+// const slideAnimation = keyframes`
+//     0% {
+//         transform: translateX(0);
+//     }
+//     100% {
+//         transform: translateX(-50%);
+//     }
+// `;
 
 const containerStyle = css`
     display: flex;
@@ -77,10 +74,10 @@ const imageContainerStyle = css`
     margin: 12px 0;
 `;
 
-const imageSlideStyle = css`
+/* const imageSlideStyle = css`
     display: flex;
     animation: ${slideAnimation} 1.5s linear infinite;
-`;
+`; */
 
 const imageStyle = css`
     width: 60px;
