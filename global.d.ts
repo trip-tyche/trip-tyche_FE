45f1182722922130f1432ac7c9e8ed30f3c25f0e
Mark Kwon <redhero8830@gmail.com>
2024-10-10 01:54:37 +0900

    fix: `global.d.ts` 파일에 `@/*` 타입 설정

--- global.d.ts
+++ global.d.ts
@@ -1,35 +1,6 @@
-// import '@emotion/react';
-
-// declare global {
-//     namespace JSX {
-//         interface IntrinsicAttributes {
-//             css?: import('@emotion/react').SerializedStyles;
-//         }
-//     }
-
-//     interface HTMLElementTagNameMap {}
-
-//     interface SVGElementTagNameMap {}
-
-//     interface DOMRect {
-//         x: number;
-//         y: number;
-//         width: number;
-//         height: number;
-//         top: number;
-//         right: number;
-//         bottom: number;
-//         left: number;
-//     }
-// }
-
-// declare module '@emotion/react' {
-//     export interface Theme {}
-// }
-
-// export {};
-
 import '@emotion/react';
+
+declare module '@/*';
 
 declare global {
     namespace JSX {
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,9 @@
 import { css } from '@emotion/react';
 
-import SocialLoginButtons from '@/components/common/button/SocialLoginButtons.tsx';
+import SocialLoginButtons from '@/components/common/button/SocialLoginButtons';
 import LogoImages from '@/components/common/LogoImages';
-import Toast from '@/components/common/Toast.tsx';
-import FightHeader from '@/components/layout/AirplaneHeader.tsx';
+import Toast from '@/components/common/Toast';
+import FightHeader from '@/components/layout/AirplaneHeader';
 import { ENV, OAUTH_URL } from '@/constants/auth';
 import theme from '@/styles/theme';
 
