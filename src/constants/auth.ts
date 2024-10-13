ef13d5c3bcf386acaab0ea213cd6b842faf70d2a
Mark Kwon <redhero8830@gmail.com>
2024-10-14 01:26:04 +0900

    fix: API_BASE_URL 배포용으로 수정

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -6,7 +6,7 @@ export const SOCIAL_LOGIN = {
 };
 
 export const ENV = {
-    BASE_URL: import.meta.env.VITE_API_BASE_URL,
-    // BASE_URL: '',
+    // BASE_URL: import.meta.env.VITE_API_BASE_URL,
+    BASE_URL: '',
     GOOGLE_MAPS_API_KEY: import.meta.env.VITE_API_GOOGLE_MAPS_API_KEY,
 };
