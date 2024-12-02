93e1d7d84688a7b89fa4647c2a66381a4e14ced6
Mark Kwon <redhero8830@gmail.com>
2024-12-02 23:21:46 +0900

    chore: Vite 설정 최적화 및 불필요한 옵션 제거
    - build 옵션 최적화 추가
    - alias 설정 개선
    - 불필요한 설정 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,4 @@
-// import * as path from 'path';
-
-// import react from '@vitejs/plugin-react';
-// import { defineConfig } from 'vite';
-// import tsconfigPaths from 'vite-tsconfig-paths';
-
-// export default defineConfig({
-//     server: {
-//         port: 3000,
-//     },
-//     plugins: [
-//         react({
-//             jsxImportSource: '@emotion/react',
-//             babel: {
-//                 plugins: ['@emotion/babel-plugin'],
-//             },
-//         }),
-//         tsconfigPaths(),
-//     ],
-//     resolve: {
-//         alias: {
-//             '@': path.resolve(__dirname, './src'),
-//             'scheduler/tracing': 'scheduler/tracing-profiling',
-//             'rollup/parseAst': 'rollup/dist/es/parseAst.js',
-//         },
-//     },
-//     optimizeDeps: {
-//         include: ['@emotion/react', '@emotion/styled'],
-//     },
-//     // build: {
-//     //     commonjsOptions: {
-//     //         transformMixedEsModules: true,
-//     //     },
-//     // },
-// });
-
-// import path from 'path';
+import { resolve } from 'path';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
@@ -43,6 +7,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
     server: {
         port: 3000,
+        open: true,
     },
     plugins: [
         react({
@@ -54,14 +19,15 @@ export default defineConfig({
         tsconfigPaths(),
     ],
     resolve: {
-        alias: {
-            '@': 'src',
-        },
+        alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
     },
     optimizeDeps: {
         include: ['@emotion/react', '@emotion/styled'],
     },
     build: {
+        target: 'esnext',
+        minify: 'esbuild',
+        sourcemap: false,
         commonjsOptions: {
             transformMixedEsModules: true,
         },
