75c5e743977f746b9a82b175076c7f48ad04d71f
Mark Kwon <redhero8830@gmail.com>
2024-12-03 01:31:34 +0900

    chore: emotion theme 타입 정의 개선
    - emotion의 Theme 인터페이스와 앱의 Theme 타입 충돌 해결

--- src/types/emotion.d.ts
+++ src/types/emotion.d.ts
@@ -1,11 +1,6 @@
 import '@emotion/react';
-import { Theme } from '@/styles/theme/theme';
+import { Theme as CustomTheme } from '@/styles/theme';
 
 declare module '@emotion/react' {
-    // export interface Theme {
-    //     colors: ColorsTypes;
-    //     fontSizes: FontSizeTypes;
-    //     heights: HeightsTypes;
-    // }
-    export interface Theme extends Theme {}
+    export interface Theme extends CustomTheme {}
 }
