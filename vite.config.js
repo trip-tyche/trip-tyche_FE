c5fedf51f169f53f9e306d9493c61dfd475bed2a
Mark Kwon <redhero8830@gmail.com>
2024-12-28 18:42:06 +0900

    chore: 초기 접속 요청 시 번들링되는 내용을 확인하는 rollup-plugin-visualizer 설치

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { resolve } from 'path';
-
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-
 export default defineConfig({
     server: {
         port: 3000,
