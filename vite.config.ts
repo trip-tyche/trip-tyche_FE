4ba5c90b53b477be00d20a530c756f975fb2348c
Mark Kwon <redhero8830@gmail.com>
2025-02-27 16:28:45 +0900

    chore: 불필요한 server 설정 삭제 (#39)

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,6 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-    server: {
-        port: 3000,
-        open: true,
-    },
     plugins: [
         react({
             jsxImportSource: '@emotion/react',
