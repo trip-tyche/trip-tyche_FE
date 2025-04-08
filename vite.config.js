ed764f2058dcdb187c0b99f3a8a0c2f63c506018
Mark Kwon <redhero8830@gmail.com>
2025-04-08 18:50:49 +0900

    fix: 개발 서버 환경에서만 HTTPS 설정이 적용되도록 Vite 설정 파일 수정

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,23 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 import fs from 'fs';
 import { resolve } from 'path';
-
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
-import { defineConfig, UserConfig } from 'vite';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-
-const KEY_PATH = 'certificates/local.triptychetest.shop-key.pem';
-const CERT_PATH = 'certificates/local.triptychetest.shop.pem';
-
-const baseConfig: UserConfig = {
+var KEY_PATH = 'certificates/local.triptychetest.shop-key.pem';
+var CERT_PATH = 'certificates/local.triptychetest.shop.pem';
+var baseConfig = {
     plugins: [
         react({
             jsxImportSource: '@emotion/react',
@@ -41,22 +49,18 @@ const baseConfig: UserConfig = {
         global: 'window',
     },
 };
-
-export default defineConfig(({ command }) => {
+export default defineConfig(function (_a) {
+    var command = _a.command;
     // 개발 서버의 경우 HTTPS 설정 추가
     if (command === 'serve') {
-        return {
-            ...baseConfig,
-            server: {
+        return __assign(__assign({}, baseConfig), { server: {
                 port: 3000,
                 host: '0.0.0.0',
                 https: {
                     key: fs.readFileSync(KEY_PATH),
                     cert: fs.readFileSync(CERT_PATH),
                 },
-            },
-        };
+            } });
     }
-
     return baseConfig;
 });
