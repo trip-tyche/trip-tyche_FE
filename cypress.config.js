a325c2aa235c07559f0471b7e404b378ee7df4d3
Mark Kwon <redhero8830@gmail.com>
2025-07-02 15:18:00 +0900

    chore: 불필요한 env 파일 생성 CI 제거

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,5 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
     e2e: {
         baseUrl: process.env.CI ? 'http://localhost:4173' : 'https://local.triptychetest.shop:3000',
-        setupNodeEvents(on, config) {},
     },
 });
